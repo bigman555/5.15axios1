31b90f5895c807f9231d1bfec054b67b4a057da6
bigman555 <535807220@qq.com>
2020-05-19 13:11:36 +0800

    5.18

---
+++ text2/pages/index/index.js
@@ -0,0 +1,61 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+   name:"刘德华",
+   EngleName:"Andy Lau, Lau Tal Wah",
+   NiceName:"华仔,华Dee, 华哥等",
+   nationality:"中国",
+   nation:"汉族",
+   constellation:"天平座",
+   blood:"AB型",
+   height:"174cm",
+   weight:"63kg",
+   birthCity:"香港新界大浦镇泰亨村",
+   dataBirth:"1961年9月27日"
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
